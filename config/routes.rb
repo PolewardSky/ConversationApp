7e2b950fdd262823d43c9900bd921d827a0e895f
James Grant <1993jamesgrant@gmail.com>
2023-03-13 17:38:56 +0000

    Create basic framework for conversation > messages

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
+  resources :conversations do
+    resources :messages, only: [:create, :destroy]
+  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  root "conversations#index"
 end
